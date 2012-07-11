9f715848614308222aed3084c059b410dc430eca
Joe Kain <jkain.dev@gmail.com>
2012-07-10 23:20:07 -0700

    [PhotoImporter] Implemented
    Implement the importer for iPhone.

--- PhotoImporter/JKPIViewController.h
+++ PhotoImporter/JKPIViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JKPIViewController : UIViewController
+@interface JKPIViewController : UIViewController <UITextFieldDelegate> {
+    NSString *url;
+    NSString *group;
+    NSOperationQueue *operationQueue;
+    bool isProcessing;
+}
+
+@property (strong, nonatomic) IBOutlet UITextField *urlField;
+@property (strong, nonatomic) IBOutlet UITextField *groupField;
+@property (strong, nonatomic) IBOutlet UIButton *goButton;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) IBOutlet UIView *greyOut;
+
+- (IBAction)go:(id)sender;
 
 @end
--- PhotoImporter/JKPIViewController.m
+++ PhotoImporter/JKPIViewController.m
@@ -6,13 +6,34 @@
 //  Copyright (c) 2012 Joseph Kain. All rights reserved.
 //
 
+#import <AssetsLibrary/ALAssetsLibrary.h>
+#import <AssetsLibrary/ALAssetsGroup.h>
+#import <AssetsLibrary/ALAsset.h>
+
 #import "JKPIViewController.h"
 
 @interface JKPIViewController ()
-
+- (void)fetchImages;
 @end
 
 @implementation JKPIViewController
+@synthesize goButton = _goButton;
+@synthesize activityIndicator = _activityIndicator;
+@synthesize greyOut = _greyOut;
+@synthesize urlField = _urlField;
+@synthesize groupField = _groupField;
+
+- (void) setBusy:(BOOL)busy
+{
+    isProcessing = busy;
+    if (busy) {
+        [self.activityIndicator startAnimating];
+    } else {
+        [self.activityIndicator stopAnimating];
+    }
+    self.goButton.enabled = !busy;
+    self.greyOut.hidden = !busy;
+}
 
 - (void)viewDidLoad
 {
@@ -22,17 +43,89 @@
 
 - (void)viewDidUnload
 {
+    [self setUrlField:nil];
+    [self setGroupField:nil];
+    [self setGoButton:nil];
+    [self setActivityIndicator:nil];
+    [self setGreyOut:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Button handle
+
+- (IBAction)go:(id)sender
+{
+    // Fetch the data from the text fields
+    url = self.urlField.text;
+    group = self.groupField.text;
+
+    // busy
+    self.busy = YES;
+    
+    // Kick off the fetch and store in a separate thread
+    operationQueue = [NSOperationQueue new];
+    [operationQueue addOperation:
+     [[NSInvocationOperation alloc] initWithTarget:self
+                                          selector:@selector(fetchImages)
+                                            object:nil]];
+}
+
+#pragma mark - thread handling
+- (void) doneThread
+{
+    [operationQueue waitUntilAllOperationsAreFinished];
+    operationQueue = nil;
+    
+    self.busy = NO;
+}
+
+- (void)fetchImages
+{
+    ALAssetsLibrary* library = [[ALAssetsLibrary alloc] init];
+
+    int i;
+    
+    // This processes images from pi-instaweb
+    for (i = 0; TRUE; i++) {
+        NSString *fullPath = [NSString stringWithFormat:@"%@/%d", url, i];
+        NSLog(@"Downloading %d\n", i);
+        
+        // Download each image
+        NSURL *nsurl = [NSURL URLWithString:fullPath];
+        NSData *data = [NSData dataWithContentsOfURL:nsurl];
+        if (!data) {
+            break;
+        }
+        
+        // Write image to photos
+        // This is assynchronous.  Wait for it to finish or it seems to overflow and then some of the images fail.
+        __block BOOL done = NO;
+        [library writeImageDataToSavedPhotosAlbum:data metadata:nil completionBlock:^(NSURL *assetURL, NSError *error) {
+            if (error) {
+                NSLog(@"Could not save image %d\n", i);
+            }
+            done = YES;
+        }];
+        while (!done);
     }
+
+    // Let the main thread know this task is done
+    [self performSelectorOnMainThread:@selector(doneThread) withObject:nil waitUntilDone:NO];
+}
+
+#pragma mark - UITextFieldDelegate
+
+// return NO to disallow editing.
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    // Disallow editing while processing
+    return !isProcessing;
 }
 
 @end
